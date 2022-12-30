9f5b875a5fdf7c55635a0d28f637b21441e78a6e
daniklean <danielvieras10@gmail.com>
2022-12-30 15:38:56 -0400

    feat(New Architecture): :safety_vest: Class base code status
    Handle errors for response code status base
    
    Breaking Changes: Add Handle error with Code Status for all module's

---
+++ app/shared/handle_Errors/http.response.ts
@@ -0,0 +1,53 @@
+import { Response } from "express"
+
+export enum codeStatus {
+    OK = 200,
+    NOT_FOUND = 404,
+    FORBIDDEN = 403,
+    UNAUTHORIZED = 401,
+    INTERNAL_SERVER_ERROR = 500
+
+}
+
+export class ResponseCodeStatus {
+
+    Success(res:Response, data?: any ): Response {
+        return res.status(codeStatus.OK).json({
+            status: codeStatus.OK,
+            statusMsg: "SUCCESS", 
+            data: data,
+        })
+    }
+
+    NotFound(res:Response, data?: any ): Response {
+        return res.status(codeStatus.NOT_FOUND).json({
+            status: codeStatus.NOT_FOUND,
+            statusMsg: "NOT FOUND", 
+            error: data,
+        })
+    }
+
+    Unauthorized(res:Response, data?: any) : Response {
+        return res.status(codeStatus.UNAUTHORIZED).json({
+            status: codeStatus.UNAUTHORIZED,
+            statusMsg: "UNAUTHORIZED", 
+            error: data,
+        })
+    }
+
+    ServerError(res:Response, data?: any) : Response {
+        return res.status(codeStatus.INTERNAL_SERVER_ERROR).json({
+            status: codeStatus.INTERNAL_SERVER_ERROR,
+            statusMsg: "INTERNAL SERVER ERROR", 
+            error: data,
+        })
+    }
+
+    Forbiddenr(res:Response, data?: any) : Response {
+        return res.status(codeStatus.FORBIDDEN).json({
+            status: codeStatus.FORBIDDEN,
+            statusMsg: "FORBIDDEN", 
+            error: data,
+        })
+    }
+}
