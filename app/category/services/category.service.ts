e3e31f8cdbf1e465e5900fc151143cb2ae67d022
daniklean <danielvieras10@gmail.com>
2023-05-19 12:30:08 -0400

    feat(New Architecture): :seedling: Category Services
    Add services in module category
    
    Breaking Changes: Added services in Module Category

--- app/category/services/category.service.ts
+++ app/category/services/category.service.ts
@@ -8,4 +8,20 @@ export class CategoryService extends BaseService<CategoryEntity> {
     constructor(){
         super(CategoryEntity)
     }
+
+    async findAllCategory(): Promise<CategoryEntity[]>{
+        return (await this.execRepository).find()
+    }
+    async findCategoryByID(id: string): Promise<CategoryEntity | null>{
+        return (await this.execRepository).findOneBy({id})
+    }
+    async createCategory(body: CategoryDTO): Promise<CategoryEntity>{
+        return (await this.execRepository).save(body)
+    }
+    async updateCategory(id: string, dataUpdate: CategoryDTO): Promise<UpdateResult>{
+        return (await this.execRepository).update(id, dataUpdate)
+   }
+    async deleteCategory(id: string): Promise<DeleteResult>{
+        return (await this.execRepository).delete({id})
+    }
 }
