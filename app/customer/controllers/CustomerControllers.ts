6bdb9b241516da695e831d6c68bd381c392f7a61
daniklean <danielvieras10@gmail.com>
2023-05-19 15:26:01 -0400

    feat(Module's): :sparkles: Add Controllers Customers
    Added controllers in Module Customers
    
    Breaking Changes: Added controllers in Customers Module

---
+++ app/customer/controllers/CustomerControllers.ts
@@ -0,0 +1,71 @@
+import { Request, Response } from "express";
+import { DeleteResult, UpdateResult } from 'typeorm'
+import { ResponseCodeStatus } from "../../shared/handle_Errors/http.response";
+import { CustomerService } from "../services/customer.service";
+
+export class CustomerController {
+    constructor(
+        private readonly customerService: CustomerService = new CustomerService,
+        private readonly status: ResponseCodeStatus = new ResponseCodeStatus 
+    ){}
+
+    async getCustomers(req: Request, res:Response) {
+        try {
+           const data = await this.customerService.findAllCustomer()
+           if(data.length === 0){
+              return this.status.NotFound(res,"Not data found")
+           }
+           return this.status.Success(res,data)
+        } catch (error:any) {
+           return this.status.ServerError(res,error)
+        }
+     }
+  
+     async getCustomerByID(req: Request, res:Response) {
+        try {
+           const { id } = req.params
+           const data = await this.customerService.findCustomerByID(id)
+           if(!data){
+              return this.status.NotFound(res,"Not exist Customer")
+           }
+           return this.status.Success(res,data)
+        } catch (error:any) {
+           return this.status.ServerError(res,error)
+        }
+     }
+  
+     async postCreateCustomer(req: Request, res:Response) {
+        try {
+           const data = await this.customerService.createCustomer(req.body)
+           res.status(200).json(data)
+        } catch (error:any) {
+           return this.status.ServerError(res,error)
+        }
+     }
+  
+     async updateCustomer(req: Request, res:Response) {
+        try {
+           const { id } = req.params
+           const data: UpdateResult = await this.customerService.updateCustomer(id, req.body)
+           if(!data.affected){
+              return this.status.NotFound(res,"Not updated data customer")
+           }
+           res.status(200).json(data)
+        } catch (error:any) {
+           return this.status.ServerError(res,error)
+        }
+     }
+  
+     async deleteCustomer(req: Request, res:Response) {
+        try {
+           const { id } = req.params
+           const data: DeleteResult = await this.customerService.deleteCustomer(id)
+           if(!data.affected){
+              return this.status.NotFound(res,"Not delete data Category")
+           }
+           res.status(200).json(data)
+        } catch (error:any) {
+           return this.status.ServerError(res,error)
+        }
+     }
+}
