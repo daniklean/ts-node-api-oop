69edc41f69ba56a747e9fa0d94f3fe0781b77011
daniklean <danielvieras10@gmail.com>
2023-05-19 15:28:50 -0400

    feat(Module's): :sparkles: Add routes customers
    Added routes in customers module
    
    Breaking Changes: Added routes in customer module

---
+++ app/customer/routes/CustomerRoutes.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { CustomerController } from "../controllers/CustomerControllers";
+import { BaseRouter } from "../../shared/router/BaseRouter";
+
+export class UserRouters extends BaseRouter<CustomerController> {
+    constructor(){
+        super(CustomerController)
+    }
+
+    routes(): void {
+        this.router.get("/customers", (req:Request, res:Response) => this.controller.getCustomers(req,res))
+        
+        this.router.get("/customer/:id", (req:Request, res:Response) => this.controller.getCustomerByID(req,res))
+
+        this.router.post("/createCustomer", (req:Request, res:Response) => this.controller.postCreateCustomer(req,res))
+        
+        this.router.put("/updateCustomer/:id", (req:Request, res:Response) => this.controller.updateCustomer(req,res))
+
+        this.router.delete("/deleteCustomer/:id", (req:Request, res:Response) => this.controller.deleteCustomer(req,res))
+    }
+}
