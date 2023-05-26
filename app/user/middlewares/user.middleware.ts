5c231d624cef070e0b04b494bb32de9c7952ff5b
daniklean <danielvieras10@gmail.com>
2023-05-26 02:03:58 -0400

    feat(Module's): :safety_vest: New middleware validator
    New middleware validator data in user module
    
    Breaking Changes: Validation Data Module User

--- app/user/middlewares/user.middleware.ts
+++ app/user/middlewares/user.middleware.ts
@@ -1,11 +1,15 @@
 import { Request, Response, NextFunction } from "express"
-import { Validate } from "class-validator"
+import { validate } from "class-validator"
 import { UserDTO } from "../dto/user.dto"
+import { ResponseCodeStatus } from "../../shared/handle_Errors/http.response"
 
 
 
 export class UserMiddleware {
-   userValidate(req: Request, res: Response, next: NextFunction) {
+   constructor( private readonly status: ResponseCodeStatus = new ResponseCodeStatus){   
+   }
+
+  async userValidate(req: Request, res: Response, next: NextFunction) {
       const {
          username,
          name,
@@ -27,5 +31,15 @@ export class UserMiddleware {
       validated.city = city
       validated.province = province
       validated.role = role
+
+      const validationProcess = await validate(validated)
+
+      if(validationProcess.length > 0) {
+         return this.status.ServerError(res,validationProcess)
+      }
+      else {
+         next()
+      }
    }
+   
 }
