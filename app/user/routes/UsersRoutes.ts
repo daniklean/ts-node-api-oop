b2dc2fc6fcfce6ad5eb178b93de0c604113a9963
daniklean <danielvieras10@gmail.com>
2023-05-26 02:12:16 -0400

    feat(Module's): :passport_control: Protected routes
    Protected routes in user module for create new user
    
    Breaking Changes: Protected routes in user module

--- app/user/routes/UsersRoutes.ts
+++ app/user/routes/UsersRoutes.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
 import { UserController } from "../controllers/UsersControllers";
 import { BaseRouter } from "../../shared/router/BaseRouter";
+import { UserMiddleware } from "../middlewares/user.middleware";
 
-export class UserRouters extends BaseRouter<UserController> {
+export class UserRouters extends BaseRouter<UserController, UserMiddleware> {
     constructor(){
-        super(UserController)
+        super(UserController, UserMiddleware)
     }
 
     routes(): void {
@@ -14,7 +15,7 @@ export class UserRouters extends BaseRouter<UserController> {
 
         this.router.get("/userCustomer/:id", (req:Request, res:Response) => this.controller.getUserWithRelationById(req,res))
 
-        this.router.post("/createUser", (req:Request, res:Response) => this.controller.postCreateUser(req,res))
+        this.router.post("/createUser", (req,res,next) => [this.middleware.userValidate(req,res,next)], (req:Request, res:Response) => this.controller.postCreateUser(req,res))
         
         this.router.put("/updateUser/:id", (req:Request, res:Response) => this.controller.updateUser(req,res))
 
