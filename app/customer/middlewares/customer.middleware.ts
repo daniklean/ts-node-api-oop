1e55bdf090586d9ffe2fc3d0d19d2a6240fd482d
daniklean <danielvieras10@gmail.com>
2023-05-26 23:51:25 -0400

    feat(Module's): :passport_control: Security with Middleware
    Security with middleware in customer module
    
    Breaking changes: Security with middleware in customer module

---
+++ app/customer/middlewares/customer.middleware.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from "express"
+import { validate } from "class-validator"
+import { ResponseCodeStatus } from "../../shared/handle_Errors/http.response"
+import { CustomerDTO } from "../dto/customer.dto"
+
+
+
+export class CustomerMiddleware {
+   constructor( private readonly status: ResponseCodeStatus = new ResponseCodeStatus){   
+   }
+
+  async customerValidate(req: Request, res: Response, next: NextFunction) {
+      const {
+        address,
+        dni
+      } = req.body
+
+      const validated = new CustomerDTO()
+
+      validated.address = address
+      validated.dni = dni
+
+      const validationProcess = await validate(validated)
+
+      if(validationProcess.length > 0) {
+         return this.status.ServerError(res,validationProcess)
+      }
+      else {
+         next()
+      }
+   }
+   
+}
