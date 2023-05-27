9f2f20cee3826e86940aeafbc3348995fe003544
daniklean <danielvieras10@gmail.com>
2023-05-27 12:30:31 -0400

    perf(Module's): :truck: Add middlware and format route
    Added middlware and format routes in category module
    
    Breaking Changes:Add middlware in endpoint create and format routes in category module

--- app/category/routes/CategoriesRoutes.ts
+++ app/category/routes/CategoriesRoutes.ts
@@ -1,21 +1,34 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { CategoryController } from "../controllers/CategoryControllers";
 import { BaseRouter } from "../../shared/router/BaseRouter";
+import { CategoryMiddleware } from "../middlewares/category.middleware";
 
-export class CategoryRouters extends BaseRouter<CategoryController> {
+export class CategoryRouters extends BaseRouter<CategoryController, CategoryMiddleware> {
     constructor(){
-        super(CategoryController)
+        super(CategoryController, CategoryMiddleware)
     }
 
     routes(): void {
-        this.router.get("/categories", (req:Request, res:Response) => this.controller.getCategories(req,res))
+        this.router.get("/categories", 
+        (req:Request, res:Response) => 
+        this.controller.getCategories(req,res))
         
-        this.router.get("/category/:id", (req:Request, res:Response) => this.controller.getCategoryByID(req,res))
+        this.router.get("/category/category/:id", 
+        (req:Request, res:Response) => 
+        this.controller.getCategoryByID(req,res))
 
-        this.router.post("/createCategory", (req:Request, res:Response) => this.controller.postCreateCategory(req,res))
+        this.router.post("category/create-category", 
+        (req: Request, res: Response, next: NextFunction) => 
+        [(this.middleware.categoryValidate(req,res,next))],
+        (req:Request, res:Response) => 
+        this.controller.postCreateCategory(req,res))
         
-        this.router.put("/updateCategory/:id", (req:Request, res:Response) => this.controller.updateCategory(req,res))
+        this.router.put("category/update-category/:id", 
+        (req:Request, res:Response) => 
+        this.controller.updateCategory(req,res))
 
-        this.router.delete("/deleteCategory/:id", (req:Request, res:Response) => this.controller.deleteCategory(req,res))
+        this.router.delete("category/delete-category/:id", 
+        (req:Request, res:Response) => 
+        this.controller.deleteCategory(req,res))
     }
 }
