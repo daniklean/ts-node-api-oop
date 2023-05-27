ee7eb1da277d6c3c2dc9341dedc1d8f112cf1e7a
daniklean <danielvieras10@gmail.com>
2023-05-26 23:58:52 -0400

    feat(Module's): :passport_control: Security with Middleware
    Security with middleware in PurchaseProduct module
    
    Breaking changes: Security with middleware in module PurchasesProduct

---
+++ app/purchase/middlewares/purchase_product.middleware.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from "express"
+import { validate } from "class-validator"
+import { ResponseCodeStatus } from "../../shared/handle_Errors/http.response"
+import {PurchasesProductsDTO} from "../dto/purchase_product.dto"
+
+export class PurchaseProductMiddleware {
+   constructor( private readonly status: ResponseCodeStatus = new ResponseCodeStatus){   
+   }
+
+  async purchaseProductValidate(req: Request, res: Response, next: NextFunction) {
+      const {
+        quantityProduct,
+        totalPrice,
+        purchase,
+        product
+      } = req.body
+
+      const validated = new PurchasesProductsDTO()
+
+      validated.quantityProduct = quantityProduct
+      validated.totalPrice = totalPrice
+      validated.purchase = purchase
+      validated.product = product
+      
+      const validationProcess = await validate(validated)
+
+      if(validationProcess.length > 0) {
+         return this.status.ServerError(res,validationProcess)
+      }
+      else {
+         next()
+      }
+   }
+   
+}
