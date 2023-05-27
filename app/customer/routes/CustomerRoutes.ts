9e1356acfd143a25cf4120377592c3724e606510
daniklean <danielvieras10@gmail.com>
2023-05-27 13:37:40 -0400

    perf(Module's): :truck: Add middleware and format routes
    Added middleware, format routes and changes code response status
    
    Breaking Changes: Add middlware, format routes and change code status in customer module

--- app/customer/routes/CustomerRoutes.ts
+++ app/customer/routes/CustomerRoutes.ts
@@ -1,23 +1,38 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { CustomerController } from "../controllers/CustomerControllers";
 import { BaseRouter } from "../../shared/router/BaseRouter";
+import { CustomerMiddleware } from "../middlewares/customer.middleware";
 
-export class CustomerRouters extends BaseRouter<CustomerController> {
+export class CustomerRouters extends BaseRouter<CustomerController, CustomerMiddleware> {
     constructor(){
-        super(CustomerController)
+        super(CustomerController, CustomerMiddleware)
     }
 
     routes(): void {
-        this.router.get("/customers", (req:Request, res:Response) => this.controller.getCustomers(req,res))
+        this.router.get("/customers", 
+        (req:Request, res:Response) => 
+        this.controller.getCustomers(req,res))
         
-        this.router.get("/customer/:id", (req:Request, res:Response) => this.controller.getCustomerByID(req,res))
+        this.router.get("/customer/customer/:id", 
+        (req:Request, res:Response) => 
+        this.controller.getCustomerByID(req,res))
 
-        this.router.get("/customerUser/:id", (req:Request, res:Response) => this.controller.getUserWithRelationById(req,res))
+        this.router.get("/customer/customer-user/:id", 
+        (req:Request, res:Response) => 
+        this.controller.getUserWithRelationById(req,res))
 
-        this.router.post("/createCustomer", (req:Request, res:Response) => this.controller.postCreateCustomer(req,res))
+        this.router.post("/customer/create-customer", 
+        (req: Request, res: Response, next: NextFunction) => 
+        [(this.middleware.customerValidate(req,res,next))],
+        (req:Request, res:Response) => 
+        this.controller.postCreateCustomer(req,res))
         
-        this.router.put("/updateCustomer/:id", (req:Request, res:Response) => this.controller.updateCustomer(req,res))
+        this.router.put("/customer/update-customer/:id", 
+        (req:Request, res:Response) => 
+        this.controller.updateCustomer(req,res))
 
-        this.router.delete("/deleteCustomer/:id", (req:Request, res:Response) => this.controller.deleteCustomer(req,res))
+        this.router.delete("customer/delete-customer/:id", 
+        (req:Request, res:Response) => 
+        this.controller.deleteCustomer(req,res))
     }
 }
