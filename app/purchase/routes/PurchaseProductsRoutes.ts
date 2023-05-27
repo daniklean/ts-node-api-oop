7308e90327b96d94b98ca5a9ee957f6a0592a5ad
daniklean <danielvieras10@gmail.com>
2023-05-27 13:48:31 -0400

    perf(Module's): :truck: Add middleware, and format routes
    Added middleware, format routes and change code status in middleware
    
    Breaking Changes: Added middleware, format routes and change code status in Purchase_Products module

--- app/purchase/routes/PurchaseProductsRoutes.ts
+++ app/purchase/routes/PurchaseProductsRoutes.ts
@@ -1,21 +1,34 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { PurchaseProductControllers } from "../controllers/PurchaseProductControllers";
 import { BaseRouter } from "../../shared/router/BaseRouter";
+import { PurchaseProductMiddleware } from "../middlewares/purchase_product.middleware";
 
-export class PurchaseProductRouters extends BaseRouter<PurchaseProductControllers> {
+export class PurchaseProductRouters extends BaseRouter<PurchaseProductControllers, PurchaseProductMiddleware> {
     constructor(){
-        super(PurchaseProductControllers)
+        super(PurchaseProductControllers, PurchaseProductMiddleware)
     }
 
     routes(): void {
-        this.router.get("/purchasesProducts", (req:Request, res:Response) => this.controller.getPurchasesProducts(req,res))
+        this.router.get("/pp/purchases-products", 
+        (req:Request, res:Response) => 
+        this.controller.getPurchasesProducts(req,res))
         
-        this.router.get("/purchaseProduct/:id", (req:Request, res:Response) => this.controller.getPurchaseProductByID(req,res))
+        this.router.get("/pp/purchase-product/:id", 
+        (req:Request, res:Response) => 
+        this.controller.getPurchaseProductByID(req,res))
 
-        this.router.post("/createPurchaseProduct", (req:Request, res:Response) => this.controller.postCreatePurchaseProduct(req,res))
+        this.router.post("/pp/create-purchase-product", 
+        (req: Request,res: Response,next: NextFunction) => 
+        [(this.middleware.purchaseProductValidate(req,res,next))],
+        (req:Request, res:Response) => 
+        this.controller.postCreatePurchaseProduct(req,res))
         
-        this.router.put("/updatePurchaseProduct/:id", (req:Request, res:Response) => this.controller.updatePurchaseProduct(req,res))
+        this.router.put("/pp/update-purchase-product/:id", 
+        (req:Request, res:Response) => 
+        this.controller.updatePurchaseProduct(req,res))
 
-        this.router.delete("/detelePurchaseProduct/:id", (req:Request, res:Response) => this.controller.deletePurchaseProduct(req,res))
+        this.router.delete("/pp/delete-purchase-product/:id", (
+        req:Request, res:Response) => 
+        this.controller.deletePurchaseProduct(req,res))
     }
 }
