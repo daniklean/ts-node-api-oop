aae1f0ae4ebc0bb9f70fb69676d800dfb755966c
daniklean <danielvieras10@gmail.com>
2023-05-26 23:49:25 -0400

    feat(Module's): :passport_control: Security with middleware
    Security with middleware in category module
    
    Breaking changes: Security MIddleware Category

---
+++ app/category/middlewares/category.middleware.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from "express"
+import { validate } from "class-validator"
+import { ResponseCodeStatus } from "../../shared/handle_Errors/http.response"
+import { CategoryDTO } from "../dto/category.dto"
+
+
+
+export class CategoryMiddleware {
+   constructor( private readonly status: ResponseCodeStatus = new ResponseCodeStatus){   
+   }
+
+  async categoryValidate(req: Request, res: Response, next: NextFunction) {
+      const {
+        categoryName,
+      } = req.body
+
+      const validated = new CategoryDTO()
+
+      validated.categoryName = categoryName
+
+      const validationProcess = await validate(validated)
+
+      if(validationProcess.length > 0) {
+         return this.status.ServerError(res,validationProcess)
+      }
+      else {
+         next()
+      }
+   }
+   
+}
