dcab9106607d9f15a98bc3e633af11034176c120
daniklean <danielvieras10@gmail.com>
2023-05-26 23:52:26 -0400

    feat(Module's): :passport_control: Security with MIddleware
    Security with middleware in Product Module
    
    Breaking Changes: Security with middleware in Product

---
+++ app/product/middlewares/product.middleware.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from "express"
+import { validate } from "class-validator"
+import { ResponseCodeStatus } from "../../shared/handle_Errors/http.response"
+import { ProductDTO } from "../dto/product.dto"
+
+
+
+export class ProductMiddleware {
+   constructor( private readonly status: ResponseCodeStatus = new ResponseCodeStatus){   
+   }
+
+  async productValidate(req: Request, res: Response, next: NextFunction) {
+      const {
+        productName,
+        description,
+        price
+      } = req.body
+
+      const validated = new ProductDTO()
+
+      validated.productName = productName
+      validated.description = description
+      validated.price = price
+
+      const validationProcess = await validate(validated)
+
+      if(validationProcess.length > 0) {
+         return this.status.ServerError(res,validationProcess)
+      }
+      else {
+         next()
+      }
+   }
+   
+}
