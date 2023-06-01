23ac885eb94adfe94d04d83d317235b25ffa5929
daniklean <danielvieras10@gmail.com>
2023-05-31 23:37:32 -0400

    feat: :necktie: Functions query to User Module
    Added new functions services to return password and username for authentication
    
    Breaking Changes: Querys Builders

--- app/user/services/user.service.ts
+++ app/user/services/user.service.ts
@@ -1,4 +1,5 @@
 import { DeleteResult, UpdateResult } from "typeorm";
+import * as bcrypt from "bcrypt"
 import { BaseService } from "../../../config/base_services/base.service";
 import { UserDTO } from "../dto/user.dto";
 import { UserEntity } from "../entities/user.entity";
@@ -21,9 +22,29 @@ export class UserService extends BaseService<UserEntity> {
         .where({ id })
         .getOne();
     }
+
     async createUser(body: UserDTO): Promise<UserEntity>{
-        return (await this.execRepository).save(body)
+        const newUser = (await this.execRepository).create(body)
+        const hashing = await bcrypt.hash(newUser.password, 10)
+        newUser.password = hashing
+        return (await this.execRepository).save(newUser)
     }
+
+    async findUserByEmail(email: string): Promise<UserEntity | null> {
+        return (await this.execRepository)
+        .createQueryBuilder('User')
+        .addSelect('user.password')
+        .where({ email })
+        .getOne()
+    }
+    async findUserByUsername(username: string): Promise<UserEntity | null> {
+        return (await this.execRepository)
+        .createQueryBuilder('User')
+        .addSelect('user.password')
+        .where({ username })
+        .getOne()
+    } 
+
     async updateUser(id: string, dataUpdate: UserDTO): Promise<UpdateResult>{
         return (await this.execRepository).update(id, dataUpdate)
     }
