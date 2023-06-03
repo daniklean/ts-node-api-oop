adb6cdf7ee39205ec6ae02595d124f8c0c11232e
daniklean <danielvieras10@gmail.com>
2023-06-02 23:26:54 -0400

    refactor: :recycle: Refactor code Services User
    Refactor code in position functions sort, and add  function find role
    
    Breaking Changes: Sort Functions and new find role

--- app/user/services/user.service.ts
+++ app/user/services/user.service.ts
@@ -1,7 +1,7 @@
 import { DeleteResult, UpdateResult } from "typeorm";
 import * as bcrypt from "bcrypt"
 import { BaseService } from "../../../config/base_services/base.service";
-import { UserDTO } from "../dto/user.dto";
+import { RoleType, UserDTO } from "../dto/user.dto";
 import { UserEntity } from "../entities/user.entity";
 
 export class UserService extends BaseService<UserEntity> {
@@ -12,15 +12,9 @@ export class UserService extends BaseService<UserEntity> {
     async findAllUser(): Promise<UserEntity[]>{
         return (await this.execRepository).find()
     }
+
     async findUserByID(id: string): Promise<UserEntity | null>{
         return (await this.execRepository).findOneBy({id})
-    }
-    async findUserWithRelation(id: string): Promise<UserEntity | null> {
-        return (await this.execRepository)
-        .createQueryBuilder("user")
-        .leftJoinAndSelect("user.customer", "customer")
-        .where({ id })
-        .getOne();
     }
 
     async createUser(body: UserDTO): Promise<UserEntity>{
@@ -30,6 +24,22 @@ export class UserService extends BaseService<UserEntity> {
         return (await this.execRepository).save(newUser)
     }
 
+    async updateUser(id: string, dataUpdate: UserDTO): Promise<UpdateResult>{
+        return (await this.execRepository).update(id, dataUpdate)
+    }
+
+    async deleteUser(id: string): Promise<DeleteResult> {
+        return (await this.execRepository).delete({id})
+    }
+
+    async findUserWithRelation(id: string): Promise<UserEntity | null> {
+        return (await this.execRepository)
+        .createQueryBuilder("user")
+        .leftJoinAndSelect("user.customer", "customer")
+        .where({ id })
+        .getOne();
+    }
+
     async findUserByEmail(email: string): Promise<UserEntity | null> {
         return (await this.execRepository)
         .createQueryBuilder('User')
@@ -37,18 +47,23 @@ export class UserService extends BaseService<UserEntity> {
         .where({ email })
         .getOne()
     }
+
     async findUserByUsername(username: string): Promise<UserEntity | null> {
         return (await this.execRepository)
         .createQueryBuilder('User')
         .addSelect('user.password')
         .where({ username })
         .getOne()
+    }
+    
+    async findUserByWithRole(id: string, role: RoleType): Promise<UserEntity | null> {
+        const user = (await this.execRepository)
+        .createQueryBuilder('User')
+        .where({ id })
+        .andWhere({ role })
+        .getOne()
+
+        return user
     } 
 
-    async updateUser(id: string, dataUpdate: UserDTO): Promise<UpdateResult>{
-        return (await this.execRepository).update(id, dataUpdate)
-    }
-    async deleteUser(id: string): Promise<DeleteResult> {
-        return (await this.execRepository).delete({id})
-    }
 }
