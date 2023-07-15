02dc20225bee2d221f92c14a166974e2d3857b58
daniklean <danielvieras10@gmail.com>
2023-07-15 12:19:32 -0400

    refactor: :recycle: Refactor identation on Data Source
    Refactor identation on Data Source
    
    Breaking Changes: Refactor identation on Data Source

--- config/base_db/data.source.ts
+++ config/base_db/data.source.ts
@@ -4,24 +4,24 @@ import { DataSourceOptions } from "typeorm";
 import { SnakeNamingStrategy } from "typeorm-naming-strategies";
 
 dotenv.config({
-    path: process.env.NODE_ENV !==  undefined ? `.${process.env.NODE_ENV.trim()}.env` : '.env'
+    path: process.env.NODE_ENV !== undefined ? `.${process.env.NODE_ENV.trim()}.env` : '.env'
 })
 
 
-const config : DataSourceOptions = {
+const config: DataSourceOptions = {
     type: 'mysql',
-            host: process.env.DB_HOST,
-            port: Number(process.env.DB_PORT),
-            username: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE,
-            entities: [__dirname + "/../../app/**/*.entity{.ts,.js}"],
-            migrations: [__dirname + "/../../app/migrations/*{.ts,.js}"],
-            synchronize: false,
-            migrationsRun: true,
-            logging:false, 
-            namingStrategy: new SnakeNamingStrategy(),
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [__dirname + "/../../app/**/*.entity{.ts,.js}"],
+    migrations: [__dirname + "/../../app/migrations/*{.ts,.js}"],
+    synchronize: false,
+    migrationsRun: true,
+    logging: false,
+    namingStrategy: new SnakeNamingStrategy(),
 }
 
 
-export const initDataSource : DataSource = new DataSource(config)
+export const initDataSource: DataSource = new DataSource(config)
